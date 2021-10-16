c66b2c5d2191ca4771059eb8dec6500c99db2c06
ramumaha <ramumaha123@gmail.com>
2021-10-17 01:40:17 +0530

    created route class

---
+++ lib/models/route.dart
@@ -0,0 +1,143 @@
+import 'stop.dart';
+class Route {
+  final int? driverNumber;
+  final int inChargeNumber;
+  final List<Stop> stops;
+  Route(this.inChargeNumber,this.stops,{this.driverNumber});
+}
+//college
+
+//Route  1
+Route route1=Route(9940363254,[ambatturEstate1,wavin1,college]);
+
+//Route 2
+Route route2=Route(9789138761,[chengalpettuOldBS,chengalpettuNewBS,rattinakinaru,thirukazhukundram,thiruporur,college]);
+
+//Route 3
+Route route3=Route(9444883970,[peravallurBS,venusGandhiStatue,peramburRlySt,jamalia,ottery,college]);
+
+//Route 4
+Route route4=Route(9444883970,[porurKumarSweets,shellPB,mugalaivakkamBS,ramapuramBS,buttRoad,kathipara,college]);
+
+//Route 5
+Route route5=Route(9789138761,[beachStation5,lightHouse5,santhomeChurch5,kutcheryRoad,luzCorner5,mylaporeTank5,college]);
+
+//Route 6
+Route route6=Route(9444883970,[beachStation6,lightHouse6,santhomeChurch6,fshoreEstate,mrcNagar,college]);
+
+//Route 7
+Route route7=Route(9444883970,[goldenFlatsBS,goldenFlatsMangaleriPark,ambatturEstate7,wavin7,college]);
+
+//Route 8
+Route route8=Route(9791179434,[p1PoliceStationPulianthope,natarajaTheatre,choolaiPO,chindadripetRamadaHotel,sathyamTheatre,royapettah,gopalapuramGround,royapettahTTKRoad,alwarpet,rtoOffice,chennaiNeelankarai,periaNeelankarai,college]);
+
+//Route 9
+Route route9=Route(9382701648,[tskNagar,collectorNagar,parkRoad,toyotaSR,maduravoyalErikarai,college]);
+
+//Route 9A
+Route route9A=Route(9789138761,[thirumangalam,rationKadai,college]);
+
+//Route 10
+Route route10=Route(8939994848,[cpwdQuarters,goldmineHotel,cmbtPark,mmda,tirunagar,kasiTheatre,ekattuthangal,college]);
+
+//Route 11
+Route route11=Route(9444883970,[peramburMarket,peramburRS,peramburBS,pattalamBS,bhuvaneswariTheatre,doveton,gangadeeswaranKoil,dhasprakash,egmore,dms,annaArivalayam11,college]);
+
+//Route 12
+Route route12=Route(9940363254,[villivakkam,icf,iynavaramRlyQtrs,jointOffice,sayani,secretriateColony,kellys,kilpaukGarden,college]);
+
+//Route 13
+Route route13=Route(9791179434,[nathamuni,k4PS,boojanvillaJn,chintamani,newAvadiRoadPB,pachiapasSignal,chetputSignal,annaArivalayam13,sietCollege13,aavin13,college]);
+
+//Route 14
+Route route14=Route(9940363254,[arumbakkamBS,panjaliammanKoil,annaArch,nelsonManickamRoad,methaNagar,choolaimedu,pushpaNagar,valluvarkottam,tNagarTPRoad,vanimahal,drNairRoad,tNagarPH,kasiArcade,cenetaphRoad,college]);
+
+//Route 15
+Route route15=Route(8939994848,[chinmayaNagar,natesanNagar,elangoNagar,avichiSchool,ramTheatre,liberty15,kodambakkamBridge,venkatnarayanaRoad,college]);
+
+//Route 16
+Route route16=Route(9382701648,[pThangalSubway,chidambaramStore,vanuvampetChurch,ulagaram,badalavinayagarTemple,madipakkamMegaMart,kovilambakkam,vellakal,medavakkamKootRoad,college]);
+
+//Route 17
+Route route17=Route(9382701648,[ramTheatre2,liberty17,powerHouse,periyarRoad,northUsmanRoad,tNagarBS17,citNagar17,saidapet17,saidapetCourt17,madhyakailash17,college]);
+
+//Route 18
+Route route18=Route(8939994848,[korattur,postOffice,annanagarWDepot,blueStar,nerkundram,college]);
+
+//Route 19
+Route route19=Route(9444883970,[valasaravakkam,kesavarthini,alwarthirunagar,virugambakkam,kkNagarPondyGH,ammanKoil,kkNagarRoundana,ashokPillar19,college]);
+
+//Route 20
+Route route20=Route(9080504181,[chindadripet,egmore2,pudupet,royapettahClockTower,royapettahHospital,policeStation,ajantha,valluvarstatue,luzCorner20,mylaporeTank20,college]);
+
+//Route 21
+Route route21=Route(9444883970,[avichiSchool2,sooriyaHospital,vadapalaniDepot,vadapalaniSignal,ashokNagarCanaraBank,ashokNagar,srinivasaTheatre,aranganathanSubway21,citNagar21,saidapetCourt21,annaUniversity,madhyakailash21,college]);
+
+//Route 22
+Route route22=Route(9382701648,[psbbSchool,doubleTank,kkNagarDepot,esiHospital,ashokPillar22,govindanRoad,mettupalayam,srinivasaTheatre2,aranganathanSubway22,citNagar22,saidapetCourt22,madhyakailash22,college]);
+
+//Route 23
+Route route23 =Route(9876543210,[jeyarajTheatre, govtBoysschool, hotelSangamam,postalColony,ayothyaMandapam,panigrahalyanamandapam,brindavanStreet,duraisamyRoad,venkatanarayanaRoad,ttd,college]);
+
+//Route 24
+Route route24=Route(9382701648,[samiyarMadam,libertyMeenakshiCollege,periyarRoadBBC,bharathiNagarNUR,tNagarBS24,citNagar24,saidapet24,kalingarArch,saidapetCourt24,indiraNagarRlySt,college]);
+
+//Route 25
+Route route25=Route(9444883970,[chepaukStadium,pycroftsRoad,rathnaCafe,triplicaneUBI,iceHouseNKT,iceHouse,kalyaniHospital,sanskritCollege,mylaporeTank25,sathyaStudio,adayarDepot,tiruvanmiyurPO25,college]);
+
+//Route 26
+Route route26=Route(9444883970,[rangarajapuramVBank,fiveLights,brindavanStreet26,duraisamyRoad26,venkatanarayanaRoad26,ttd26,nandanam,mcDonaldsVelachery,college]);
+
+//Route 27
+Route route27=Route(9677012278,[bssHospital,mrtsBS,sangeetha,mgrJanakiCollege,adyarDepot,tiruvanmiyurPO27,college]);
+
+//Route 28
+Route route28=Route(9940363254 ,[kalpakkam,babaTempleFountainStop,anupuram,anupuramEnd,pooncheri,thiruporurBS,kalavakkam,college]);
+
+//Route 29
+Route route29=Route(9789138761,[anagaputhurBS,ammanKoil29,pammal,church,krishnaNagarBS,puthuKoil,pallavaram,pondsESI,sanitorium,perungalathur,college]);
+
+//Route 30
+Route route30=Route(9677012278,[churchParkConvent,stellamaris,musicAcademy,alwarpetNarathaganaSabha,cpRamasamyRoad,parkSheraton,madhyakailash30,college]);
+
+//Route 30A
+Route route30A=Route(9677012278,[ega,chetputSignal30A,shastriBhavan,sterlingRoadJnGemini,annaArivalayam30A,sietCollege30A,kotturpuram,iitMadras,kumaranNagarPB,college]);
+
+//Route 31
+Route route31=Route(9444883970,[idPark,saravanaSquare,sriKrishnaSweets,badalavinayagarTemple31,uti,keelkattalaiBS,sKolathur,college]);
+
+//Route 32
+Route route32=Route(9444883970,[hastinapuramBS,ammanKoil32,kumaraKundram,nehrunagarBS,mit,tpHospital,sanitoriumGKHotel,vandalurRlyGate,college]);
+
+//Route 33
+Route route33=Route(9382701648,[stThomasMountJeyalakshmiTheatre,surenderNagar,oilMill,rajendraStores,utiBank,ponniammanKoil,baliayaGarden,sadasivaNagar,ramNagar,srinivasaNagar,okkiam,college]);
+
+//Route 34
+Route route34=Route(9080504181,[adambakkamPKoil,vandikaranSt,kakkanBridge,balajiDental,narayanapuramPallikaranai,college]);
+
+//Route 35
+Route route35=Route(9677012278,[aavinoppMalarH,avvaiHome,rajajiBhavan,besantNagarDepot,vannandurai,jayanthiTheatreThiruvanmiyur,thiruvanmiyurTMBBank,vettuvankaniChurch,sholinganallurJnECR,college]);
+
+//Route 36
+Route route36=Route(9677012278,[chromepet,hindumissionHospital,erimbuliyur,perungalathur36,college]);
+
+//Route 37
+Route route37=Route(9791179434,[ambikkaAppalam,indiraNagarPk,indiraNagarCpwd,srinivasapuramBataShowroom,palavakkamAntonyChurch,neelankaraiBuhariHotel,echambakkamBusStop,uthandiTollGate,kannathur,college]);
+
+//Route 38
+Route route38=Route(9677261980,[jayanthiTheatre,kandanchavadi,perungudiTollgate,jainCollege,mettukuppam,ptcQtrs,okkiampettai,dollarBiscut,navallurCognizant,sipcot,padurBS,hindustanCollege,chettinad,kelambakkam,college]);
+
+//Route 39
+Route route39=Route(9382701648,[guindyRS,gurunanakCollege,dandeeswaran,vijayanagar,tansiNagar,babyNagar,taramaniChurch,kalyanamandapam,college]);
+
+//Route 40
+Route route40=Route(9677012278,[nesapakkamGoldenJublieBlock,velachery100RoadSangeethaHotel,erikarai,muruganKM,vijayanagarBS,kaiveli,oilMill40,asanCollege,college]);
+
+//Route 41
+Route route41=Route(9791179434,[eastTambaramUCOBank,aadhiNagarBS,selaiyurPS,coromandelFlats,campRoad,mahalakshmiNagar,kamarajapuram,gowrivakkam,medavakkamKootRoad41,college]);
+
+//Route 42
+Route route42=Route(9080504181,[devarajNagarBharathEngg,indiraNagar,campRoad42,rajakilpakkam,sembakkam,santhosapuram,puthukoil,medavakkam,perumbakkam,globalBS,college]);
+
+//Route 43
+Route route43=Route(9789138761,[singaperumalKoil,maraimalaiNagarBS,katangalathur,potheri,guduvanchery,eb,urapakkmTeakadai,urapakkamSchool,vandalurZoo,kolapakkam,tagoreEnggCollege,kandigai,mambakkam,pudupakkam,kelambakkam43,college]);
